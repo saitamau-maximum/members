49c7142089cde17b3bc6b778e36309545d3d6bc8
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-01-13 12:27:31 +0900

    完全(?) Web Standard

--- messenger/src/gh-signature.ts
+++ messenger/src/gh-signature.ts
@@ -1,14 +1,34 @@
-import { Buffer } from 'node:buffer';
-import node_crypto from 'node:crypto';
+// https://docs.github.com/ja/webhooks/using-webhooks/validating-webhook-deliveries#javascript-example
 
-// https://docs.github.com/ja/webhooks/using-webhooks/validating-webhook-deliveries#typescript-example
-const verify_signature = (req: Request, secret: string) => {
-	const signature = node_crypto.createHmac('sha256', secret).update(JSON.stringify(req.body)).digest('hex');
+const hex2bytes = (hex: string) => {
+	const len = hex.length / 2;
+	const bytes = new Uint8Array(len);
 
-	let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
-	let untrusted = Buffer.from(req.headers.get('X-Hub-Signature-256'), 'ascii');
+	for (let i = 0; i < hex.length; i += 2) {
+		const c = hex.slice(i, i + 2);
+		let b = parseInt(c, 16);
+		bytes[i / 2] = b;
+	}
 
-	return node_crypto.timingSafeEqual(trusted, untrusted);
+	return bytes;
+};
+
+const verify_signature = async (req: Request, secret: string) => {
+	const encoder = new TextEncoder();
+
+	if (!req.headers.has('X-Hub-Signature-256')) {
+		return false;
+	}
+
+	const sigHex = req.headers.get('X-Hub-Signature-256')!.split('=')[1];
+
+	const keyBytes = encoder.encode(secret);
+	const key = await crypto.subtle.importKey('raw', keyBytes, { name: 'HMAC', hash: { name: 'SHA-256' } }, false, ['sign', 'verify']);
+
+	const sigBytes = hex2bytes(sigHex);
+	const bodyBytes = encoder.encode(JSON.stringify(req.body));
+
+	return await crypto.subtle.verify('HMAC', key, sigBytes, bodyBytes);
 };
 
 export default verify_signature;
