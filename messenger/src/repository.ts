3f02e9176c8db2ba7aac2f1d4fa4fd0cb31a553a
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-04-08 18:10:41 +0900

    PR Normalizerをmessengerへ移植 (#58)
    * refactor: 処理が簡潔に読めるように分割
    
    * feat: PRのタイトルを変更する機能を追加
    
    * ci: 既存のPRタイトル変更workflowを削除
    
    * fix: PR提出時のメンバー状態のチェック関数を修正
    
    * fix: 継続時のタイトルを修正
    
    Co-authored-by: Asa <26807394+a01sa01to@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Asa <26807394+a01sa01to@users.noreply.github.com>

---
+++ messenger/src/repository.ts
@@ -0,0 +1,58 @@
+import { App } from 'octokit';
+
+export class GithubPullRequestRepository {
+	readonly owner = 'saitamau-maximum';
+	readonly repo = 'members';
+	readonly octokit: App['octokit'];
+	readonly issue_number: number;
+
+	constructor(octokit: App['octokit'], issue_number: number) {
+		this.octokit = octokit;
+		this.issue_number = issue_number;
+	}
+
+	// main ブランチに /members/{sender}.json が存在していて、isActive が false なら継続者(continuing)である。
+	// main ブランチに /members/{sender}.json が存在していて、isActive が true なら更新なし(no-updates)である。
+	// main ブランチに /members/{sender}.json が存在していない場合は新規入会者(newbie)である。
+	async checkMemberStatus(sender: string) {
+		try {
+			const res = await this.octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+				owner: this.owner,
+				repo: this.repo,
+				path: `members/${sender}.json`,
+			});
+
+			if (!Array.isArray(res.data) && res.data.type === 'file') {
+				const jsonContent = JSON.parse(atob(res.data.content));
+				if (!jsonContent.isActive) {
+					return 'continuing' as const;
+				}
+			}
+			return 'no-updates' as const;
+		} catch (e) {
+			return 'newbie' as const;
+		}
+	}
+
+	// Pull Request にメッセージを送信する
+	async sendMessages(messages: string[]) {
+		for (const message of messages) {
+			await this.octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+				owner: this.owner,
+				repo: this.repo,
+				issue_number: this.issue_number,
+				body: message,
+			});
+		}
+	}
+
+	// Pull Request のタイトルを更新する
+	async updatePullRequestTitle(title: string) {
+		await this.octokit.request('PATCH /repos/{owner}/{repo}/issues/{issue_number}', {
+			owner: this.owner,
+			repo: this.repo,
+			issue_number: this.issue_number,
+			title,
+		});
+	}
+}
